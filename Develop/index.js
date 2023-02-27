9101226064dedb593de10321ee99c5d3e914c731
axelirias@rocketmail.com <axelirias@Axels-MacBook-Air.local>
2023-02-26 21:57:05 -0700

    added questions for readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,51 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const badge = require ("./utils/generateMarkdown");
+const { type } = require('os');
+
+// function to creat readme file
+
+const readGen = ({ title, bio, license, github, email}) =>
+`
+`
+
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "What is your project name?",
+     },
+        
+     {
+        type: "input",
+        name: "bio",
+        message: "Give a quick description of your application",
+     },
+     {
+        type: "checkbox",
+        name: "license",
+        message: "Choose a license type",
+        choices: ["MIT", "ISC", "zLib-License", "Mozilla Public License 2.0"],
+     },
+     {
+        type: "input",
+        name: "github",
+        message: "What is your github username?",
+     },
+     {
+        type: "input",
+        name: "email",
+        message: "What is your email?",
+     },
+     
+    ])
+    .then((answers) =>{
+        const generateRead = readGen(answers);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+        fs.writeFile("readme.md", generateRead, (err) =>
+        err ? console.log(err) : console.log("Success new readme created!")
+        );
+    });
