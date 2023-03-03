77eb59a9adb135197e9ab1ee7743cd6903680de4
axelirias@rocketmail.com <axelirias@Axels-MacBook-Air.local>
2023-03-02 18:21:13 -0700

    finalzing

--- Develop/index.js
+++ Develop/index.js
@@ -10,52 +10,52 @@ const generateMarkdown = require ("./utils/generateMarkdown");
 inquirer
     .prompt([
       {
-        type: "input",
-        name: "title",
-        message: "What is your project name?",
+        type: 'input',
+        name: 'title',
+        message: 'What is your project name?',
      },
      {
-       type: "input",
-       name: "installation",
-       message: "What did you use to accomplish this?"
+       type: 'input',
+       name: 'installation',
+       message: 'What did you use to accomplish this?'
      },
         
      {
-        type: "input",
-        name: "bio",
-        message: "Give a quick description of your application",
+        type: 'input',
+        name: 'bio',
+        message: 'Give a quick description of your application',
      },
      {
-      input: "input",
-      name: "usage",
-      message: "what useage should people have for this"
+      input: 'input',
+      name: 'usage',
+      message: 'what useage should people have for this'
      },
      {
-      type: "input",
-      name: "dependecies",
-      message: "Which command should the user use to insall dependecies"
+      type: 'input',
+      name: 'dependecies',
+      message: 'Which command should the user use to insall dependecies'
      },
      {
-        type: "checkbox",
-        name: "license",
-        message: "Choose a license type",
+        type: 'checkbox',
+        name: 'license',
+        message: 'Choose a license type',
         choices: ["MIT", "ISC", "zLib-License", "Mozilla Public License 2.0"],
      },
      {
-        type: "input",
-        name: "github",
-        message: "What is your github username?",
+        type: 'input',
+        name: 'github',
+        message: 'What is your github username?',
      },
      {
-        type: "input",
-        name: "email",
-        message: "What is your email?",
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?',
      },
      
     ])
-    .then((answers) =>{
-
-        fs.writeFile("README.md", generateMarkdown(answers), (err) =>
+    .then((data) =>{
+         const readAns = generateMarkdown(data)
+        fs.writeFile("README.md", readAns, (err) =>
         err ? console.log(err) : console.log("Success new readme created!")
         );
     });
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,19 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch(license) {
+    case 'MIT' :
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+    case 'ISC' :
+      return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`
+    case 'zLib-license' :
+      return `[![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)](https://opensource.org/licenses/Zlib)`
+    case 'Mozilla Public License 2.0' :
+      return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`    
+
+    }
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -35,8 +48,8 @@ function renderLicenseSection(license) {}
     ${dependecies}
 
     ## License
-    ${license}
-
+    
+    [![License][https://img.shields.io/badge/License-${license}-brightgreen.svg)][https://opensource.org/licenses/${license}]
     ## Github
     ${github}
 
